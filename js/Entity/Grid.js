7a65d808c0cde88ce78c1c8ef74da0221e55d1f0
kouHEY <51882041+kouheyHEY@users.noreply.github.com>
2023-09-14 08:18:43 +0900

    Grid.js の更新

--- js/Entity/Grid.js
+++ js/Entity/Grid.js
@@ -7,9 +7,14 @@ class Grid {
 
         // グリッドの初期化
         this.initGrid();
+        this.createGrid();
     }
 
     initGrid() {
+        this.icons = [];
+    }
+
+    createGrid() {
         // グリッドにアイコンを配置する処理を追加
         for (let row = 0; row < this.numRows; row++) {
             for (let col = 0; col < this.numCols; col++) {
@@ -26,4 +31,6 @@ class Grid {
     getRandomIconType() {
         // ランダムなアイコンの種類を取得する処理を追加
     }
+
+
 }
