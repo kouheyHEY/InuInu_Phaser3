b90d51d98d4307f09e36c28c7fb008fb476b5c2a
kouheyHEY <vudtarm@gmail.com>
2023-09-28 00:05:30 +0900

    update deleteMethods

--- js/Entity/Grid.js
+++ js/Entity/Grid.js
@@ -27,20 +27,13 @@ class Grid {
      * グリッドを生成する
      */
     createGrid() {
-        let gridX = (SCR_WIDTH - this.numCols * ICON.WIDTH) / 2;
-        let gridY = (SCR_HEIGHT - this.numRows * ICON.HEIGHT) / 2;
         // グリッドにアイコンを配置する処理を追加
         for (let row = 0; row < this.numRows; row++) {
             let iconsTmp = [];
             let deleteFlgTmp = [];
             for (let col = 0; col < this.numCols; col++) {
-                const x = col * ICON.WIDTH + gridX + ICON.WIDTH / 2;
-                const y = row * ICON.HEIGHT + gridY + ICON.HEIGHT / 2;
-
-                // ランダムなアイコンの種類を取得
-                const iconType = this.getRandomIconType();
                 // アイコンを配列に追加
-                const icon = new Icon(this.scene, x, y, ICONTYPE[iconType], iconType, row, col, this);
+                const icon = this.createIcon(row, col, this.getRandomIconType());
                 iconsTmp.push(icon);
 
                 // アイコンの消去フラグ配列を初期化
@@ -63,6 +56,37 @@ class Grid {
     }
 
     /**
+     * 消去された場所にアイコンを生成する
+     */
+    generateIcons() {
+        for (let i = 0; i < this.numRows; i++) {
+            for (let j = 0; j < this.numCols; j++) {
+                if (this.icons[i][j] == null) {
+                    this.icons[i][j] = this.createIcon(i, j, this.getRandomIconType());
+                }
+            }
+        }
+    }
+
+    /**
+     * アイコンを所定の位置に生成する
+     * @param {int} _row 生成する行
+     * @param {int} _col 生成する列
+     * @param {int} _iconType アイコンの種類
+     * @returns 生成したアイコン
+     */
+    createIcon(_row, _col, _iconType) {
+        let gridX = (SCR_WIDTH - this.numCols * ICON.WIDTH) / 2;
+        let gridY = (SCR_HEIGHT - this.numRows * ICON.HEIGHT) / 2;
+
+        let x = _col * ICON.WIDTH + gridX + ICON.WIDTH / 2;
+        let y = _row * ICON.HEIGHT + gridY + ICON.HEIGHT / 2;
+
+        let icon = new Icon(this.scene, x, y, ICONTYPE[_iconType], _iconType, _row, _col, this);
+        return icon;
+    }
+
+    /**
      * 消去するアイコンの配列を設定し、消去する
      * @param {int} _selectRow アイコンの行
      * @param {int} _selectCol アイコンの列
@@ -75,11 +99,15 @@ class Grid {
         this.deleteFlg[_selectRow][_selectCol] = 1;
         this.setDeleteFlg(_selectRow, _selectCol, _type);
 
+        // アイコンを消去する
         deleteIconNum = this.deleteIcons();
 
         this.gameManager.countDeleteDog(deleteType, deleteIconNum);
 
+        // アイコンを生成する
+        this.generateIcons();
     }
+
     /**
      * アイコンの消去を再帰的に判断する
      * @param {int} _selectRow アイコンの行
@@ -129,7 +157,10 @@ class Grid {
             for (let j = 0; j < this.numCols; j++) {
                 if (this.deleteFlg[i][j] == 1) {
                     this.deleteFlg[i][j] = 0;
-                    this.icons[i][j].destroy();
+
+                    let deleteIcon = this.icons[i][j];
+                    this.icons[i][j] = null;
+                    deleteIcon.destroy();
                     deleteNum++;
                 }
             }
