3b1695d3f01ddaefd98e21673cec382426ed7ac9
kouheyHEY <vudtarm@gmail.com>
2023-10-21 00:48:12 +0900

    make fieldManager

---
+++ js/Act1/Manager/FieldManager.js
@@ -0,0 +1,12 @@
+class FieldManager {
+    constructor(scene) {
+
+        // フィールドを表す配列
+        this.field = [];
+        // シーン
+        this.scene;
+        // フィールド上の足場のグループ
+        this.groundGroup = this.scene.physics.add.group();
+    }
+
+}
